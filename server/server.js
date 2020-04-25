2b53f1da2d90896f9e6bf8a6dbc253a4e1cedf0d
Kelly online <kellyonline2016@gmail.com>
2020-04-25 11:44:13 +0100

    added json file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 8000;
 
+var json = { count : 10, message : 'hi json'}
+
+app.get('/', (req, res) => {
+
+})
 app.listen(port, () => {
     console.log(`Server is currently running on https://localhost:${port}`);
     

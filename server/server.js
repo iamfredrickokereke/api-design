3072753640e9180879d7279096bdffa7f87b02b3
Kelly online <kellyonline2016@gmail.com>
2020-04-25 11:49:39 +0100

    added nodemon scripts

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const port = 8000;
 var json = { count : 10, message : 'hi json'}
 
 app.get('/', (req, res) => {
-
+    res.json(json)
+    console.log(json);
+    
 })
 app.listen(port, () => {
     console.log(`Server is currently running on https://localhost:${port}`);
